6b4e38dae34c72b4e835a038d7b3ac0e7fe6347b
Jiang.Shang <jshang@fordham.edu>
2019-08-22 12:01:45 -0400

    initiate a practice.go script to test my understanding

---
+++ hello/practice.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+
+
+var a int = 1
+
+func main(){
+	fmt.Println(a)
+}
+
+
+
+// var (
+// 	a = 1
+// 	b = 2
+// 	c = 3
+// )
+
+// func main() {
+// 	fmt.Println("a is ", a)
+// 	fmt.Println("b is ", b)
+// 	fmt.Println("c is ", c)
+// }
